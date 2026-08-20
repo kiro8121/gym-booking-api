05a32851177ee2cb641e828563d3ab70b0e7d90b
karen <karenmilad06@gmail.com>
2026-08-20 17:46:09 +0300

    Add Swagger setup

---
+++ src/config/swagger.ts
@@ -0,0 +1,16 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+export const swaggerOptions : swaggerJSDoc.Options = {
+ definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Gym Management API",
+      version: "1.0.0",
+      description: "API for managing Gym's classes and bookings.",
+    },
+  },
+
+  apis: ["./routes/*.ts"],
+};
+
+export const specs=swaggerJSDoc(swaggerOptions);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { connectDB } from "./config/db";
-
+import { specs } from "./config/swagger";
 dotenv.config();
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/", (req, res) => {
     res.send("Gym Booking API is running...");
