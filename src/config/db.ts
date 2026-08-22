1dd757e656345614f96701a81427acff201bb35e
Abanoub-crypto <Abanob.filmon@gmail.com>
2026-08-22 10:45:21 +0300

    done

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(
-            "mongodb://byhxkiro_db_user:4jQUgBjdAcFQaHFv@ac-ecwe4fm-shard-00-00.vscw21w.mongodb.net:27017,ac-ecwe4fm-shard-00-01.vscw21w.mongodb.net:27017,ac-ecwe4fm-shard-00-02.vscw21w.mongodb.net:27017/gym_booking?ssl=true&replicaSet=atlas-mizdj1-shard-0&authSource=admin&appName=Cluster0"
-        );
+        const mongoUri = process.env.MONGO_URI;
+        if (!mongoUri) {
+            throw new Error("MONGO_URI is not defined in environment variables");
+        }
+
+        const conn = await mongoose.connect(mongoUri);
         console.log(`MongoDB Connected Successfully: ${conn.connection.host}`);
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
